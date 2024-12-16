eecb5b1e585668d9ea8b00af2d20a05742f60d54
booleto <exaccxd@gmail.com>
2024-12-16 23:15:18 +0700

    config and add timelines

--- script/level_manager/ThanhDiaTuLinh.gd
+++ script/level_manager/ThanhDiaTuLinh.gd
@@ -1,2 +1,18 @@
 extends Node2D
 class_name ThanhDiaTuLinh
+
+var found_door: bool = false
+
+func _ready() -> void:
+	GameEventBus.play_animation.emit("arrows")
+	Dialogic.start("tdtl_start")
+	GameEventBus.game_event.connect(_on_game_event)
+
+func _on_game_event(event: String):
+	if event == "door_tu" and not found_door:
+		Dialogic.start("tdtl_correct_door")
+		found_door = true
+	if event == "door_tu" and found_door:
+		Dialogic.start("tdtl_correct_door_short")
+
+	
--- script/ui/button/ButtonHold.gd
+++ script/ui/button/ButtonHold.gd
@@ -5,7 +5,8 @@ class_name ButtonHold
 @export var on_release: ButtonAction
 
 func _ready() -> void:
-	assert(action != null, "This button has no action assigned to it!")
+	assert(on_press != null, "This button doesn't have starting action assigned to it!")
+	assert(on_release != null, "This button doesn't have release action assigned to it!")
 	assert(self is BaseButton, "Button script assigned to something that's not a button!")
 	self.button_down.connect(press)
 	self.button_up.connect(release)
