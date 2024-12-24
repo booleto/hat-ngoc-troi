13b1d3112074f4922bb306afb1105a0f3547d112
Nguyễn Anh Vũ <101244686+3Eye18@users.noreply.github.com>
2024-12-24 15:40:52 +0700

    Finishing mission tab

--- scene/dialogic/style/mainstyle/MissionTab/mission_tab.gd
+++ scene/dialogic/style/mainstyle/MissionTab/mission_tab.gd
@@ -1,10 +1,17 @@
 extends DialogicLayoutLayer
+class_name MissionTab
 
+@onready var label = %Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Dialogic.signal_event.connect(_on_event)
+	GameEventBus.game_event.connect(_on_game_event)
+	Dialogic.signal_event.connect(_on_game_event)
 
-
-func _on_event(event: String):
-	pass
+# Update the mission tab when it's already added to the scene
+func _on_game_event(event: String) -> void:
+	print("MT Received Event: ", event)
+	if event == "card_pressed":
+		print(Dialogic.VAR.card_count)
+		label.text = "Tìm các tấm thẻ gỗ (" + str(Dialogic.VAR.card_count) + "/6)"
+	
--- script/level_manager/LevelPhuong.gd
+++ script/level_manager/LevelPhuong.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
 @onready var card_panel_scene = preload("res://scene/minigame/drag_cards/card_sort_panel.tscn")
+@onready var mission_tab_scene = preload("res://scene/dialogic/style/mainstyle/MissionTab/mission_tab.tscn")
 @onready var main_panel = %MainPanel
 var card_list: ReorderableContainer = null
 var card_panel: CardSortPanel = null
+var mission_tab: MissionTab = null
 
 const correct_order: Array[String] = [
 	"LuaKho",
@@ -23,6 +25,12 @@ func _ready() -> void:
 
 func _on_game_event(event: String) -> void:
 	print("Received Event: ", event)
+	if event == "find_card_start":
+		start_find_card_game()
+	
+	if event == "find_card_done":
+		end_find_card_game()
+	
 	if event == "sort_start":
 		start_sort_game()
 		
@@ -35,7 +43,24 @@ func _on_game_event(event: String) -> void:
 			end_sort_game_fail()
 
 
+# Add the mission tab when the find card minigame start
+func start_find_card_game() -> void:
+	mission_tab = mission_tab_scene.instantiate()
+	main_panel.add_child(mission_tab)
+	mission_tab.label.text = "Tìm các tấm thẻ gỗ (" + str(Dialogic.VAR.card_count) + "/6)"
+	
+# Delet the mission tab when the find card minigame start
+func end_find_card_game() -> void:
+	mission_tab.queue_free()
+	await mission_tab.tree_exited
+	mission_tab = null
+
 func start_sort_game() -> void:
+	mission_tab = mission_tab_scene.instantiate()
+	main_panel.add_child(mission_tab)
+	mission_tab.z_index = 6
+	mission_tab.label.text = "Hãy sắp xếp các tấm thẻ theo đúng thứ tự"
+	
 	card_panel = card_panel_scene.instantiate()
 	main_panel.add_child(card_panel)
 	card_panel.show()
@@ -45,9 +70,16 @@ func start_sort_game() -> void:
 
 func end_sort_game_fail() -> void:
 	card_list = null
+	# Destroy node and set card panel to null
 	card_panel.queue_free()
 	await card_panel.tree_exited
 	card_panel = null
+
+	# The same for mission tab
+	mission_tab.queue_free()
+	await mission_tab.tree_exited
+	mission_tab = null
+	
 	Dialogic.start("cards_sort_fail")
 
 
@@ -55,9 +87,17 @@ func end_sort_game() -> void:
 	GameEventBus.play_animation.emit("mission_completed")
 	await get_tree().create_timer(2.0).timeout
 	card_list = null
+	
+	# Destroy node and set card panel to null
 	card_panel.queue_free()
 	await card_panel.tree_exited
 	card_panel = null
+
+	# The same for mission tab
+	mission_tab.queue_free()
+	await mission_tab.tree_exited
+	mission_tab = null
+	
 	Dialogic.start("cards_sorted")
 	pass
 
