b5861f5ac0277fa4c96332ab8d6314dfae07af51
booleto <exaccxd@gmail.com>
2025-01-04 11:45:18 +0700

    basic interactions for quy level

--- script/level_manager/LevelQuy.gd
+++ script/level_manager/LevelQuy.gd
@@ -1,4 +1,24 @@
 extends Node
 
+@onready var statue_right_panel : PanelContainer = %StatueRightPanel
+@onready var statue_left_panel : PanelContainer = %StatueLeftPanel
+
 func _ready() -> void:
+	GameEventBus.play_animation.emit("arrows")
+	Dialogic.signal_event.connect(_on_game_event)
+	GameEventBus.game_event.connect(_on_game_event)
+	
+func _on_game_event(event: String):
+	print_debug("Received Event: ", event)
+	
+	if event == "view_statue_right":
+		statue_right_panel.show()
+	if event == "hide_statue_right":
+		statue_right_panel.hide()
+	if event == "view_statue_left":
+		statue_left_panel.show()
+	if event == "hide_statue_left":
+		statue_left_panel.hide()
+		
+	
 	pass
